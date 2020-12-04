96cac5e4ef9210d402226879525140e1c8245a12
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-12-04 13:48:56 +0100

    searching books

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(()=>{
+    const form = document.querySelector(".search-form");
+    const input = document.querySelector("#search");
+
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        loadBooks(input.value);
+    })
+  })
+  const fetchData = async (name) => {
+    var books2 = []
+    await fetch(`https://www.googleapis.com/books/v1/volumes?q=${name}`)
+    .then(response =>{
+        if(response.ok){
+            return response.json()
+        } else {
+            return Promise.reject(response)
+        }
+    })
+    .then(response => {
+        books2 = response.items
+    })
+    .catch(error => {
+        console.error("Błąd pobrania API!")
+        books2 = false;
+    })
+    console.log(books2)
+    return books2
+}
+const loadBooks = async (name) => {
+  const books = await fetchData(name)
+  var booksOnSite = document.querySelector('.ksiazki')
+  var booksToAdd = ""
+  console.log("przed wejsciem do books")
+  if(books) {
+      console.log(books[1].volumeInfo)
+      books.map(item=>booksToAdd = booksToAdd + "<div>" + item.volumeInfo.title +"<div>")
+      booksOnSite.innerHTML = booksToAdd
+  }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form" id="searchForm">
+        <input type="text" id="search"/>
+        <button type="submit">Szukaj</button>
+      </form>
+      <div className="ksiazki">
+
+      </div>
     </div>
   );
 }
